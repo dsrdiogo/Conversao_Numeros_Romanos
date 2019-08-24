9508b40f8a276cd910a7d594b227cd7d8a0e86c8
dsrdiogo <49886056+dsrdiogo@users.noreply.github.com>
2019-08-24 11:24:11 -0300

    Add files via upload

---
+++ TraducaoNumerosRomanos_ver2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TraducaoNumerosRomanos_ver2
+{
+    partial class Frm_Conversor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Converter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txt_Resultado = new System.Windows.Forms.TextBox();
+            this.Txt_Numero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn_Converter
+            // 
+            this.Btn_Converter.Location = new System.Drawing.Point(208, 27);
+            this.Btn_Converter.Name = "Btn_Converter";
+            this.Btn_Converter.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Converter.TabIndex = 0;
+            this.Btn_Converter.Text = "Converter";
+            this.Btn_Converter.UseVisualStyleBackColor = true;
+            this.Btn_Converter.Click += new System.EventHandler(this.Btn_Converter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Números Romanos";
+            // 
+            // Txt_Resultado
+            // 
+            this.Txt_Resultado.Location = new System.Drawing.Point(6, 75);
+            this.Txt_Resultado.Name = "Txt_Resultado";
+            this.Txt_Resultado.Size = new System.Drawing.Size(277, 20);
+            this.Txt_Resultado.TabIndex = 2;
+            // 
+            // Txt_Numero
+            // 
+            this.Txt_Numero.Location = new System.Drawing.Point(6, 30);
+            this.Txt_Numero.Name = "Txt_Numero";
+            this.Txt_Numero.Size = new System.Drawing.Size(196, 20);
+            this.Txt_Numero.TabIndex = 3;
+            this.Txt_Numero.TextChanged += new System.EventHandler(this.Txt_Numero_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Txt_Numero);
+            this.groupBox1.Controls.Add(this.Btn_Converter);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Txt_Resultado);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // Frm_Conversor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 110);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Frm_Conversor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Converter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt_Resultado;
+        private System.Windows.Forms.TextBox Txt_Numero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ TraducaoNumerosRomanos_ver2/Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TraducaoNumerosRomanos_ver2
+{
+    public partial class Frm_Conversor : Form
+    {
+        public Frm_Conversor()
+        {
+            InitializeComponent();
+        }
+
+        List<string> _listaLetrasRomanas;
+        List<int> _listaNumerosInteiros;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            _listaLetrasRomanas = new List<string>();
+            _listaNumerosInteiros = new List<int>();
+
+            _listaLetrasRomanas.Add("I");
+            _listaLetrasRomanas.Add("V");
+            _listaLetrasRomanas.Add("X");
+            _listaLetrasRomanas.Add("L");
+            _listaLetrasRomanas.Add("C");
+            _listaLetrasRomanas.Add("D");
+            _listaLetrasRomanas.Add("M");
+
+            _listaNumerosInteiros.Add(1);
+            _listaNumerosInteiros.Add(5);
+            _listaNumerosInteiros.Add(10);
+            _listaNumerosInteiros.Add(50);
+            _listaNumerosInteiros.Add(100);
+            _listaNumerosInteiros.Add(500);
+            _listaNumerosInteiros.Add(1000);
+
+        }
+
+        private void Txt_Numero_TextChanged(object sender, EventArgs e)
+        {
+
+            if (Txt_Numero.Text == "")
+            {
+
+                //MessageBox.Show("Informme o Número Romano que deseja converter!", "Conversor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+
+            }
+
+            int inicio = 0;
+            bool resultado = false;
+
+            if (Txt_Numero.Text.Length > 1)
+            {
+                inicio = Txt_Numero.Text.Length - 1;
+            }
+            else
+            {
+                inicio = 0;
+            }
+
+            int i = 0;
+            for (i = 0; i <= _listaLetrasRomanas.Count - 1; i++)
+            {
+
+                if (_listaLetrasRomanas[i] == Txt_Numero.Text.Substring(inicio, 1).ToUpper())
+                {
+                    resultado = true;
+                    break;
+                }
+                else
+                {
+                    resultado = false;
+                }
+
+            }
+
+            if (resultado == false)
+            {
+                MessageBox.Show("A letra informada não é válida!", "Conversor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Txt_Numero.Clear();
+                Txt_Resultado.Clear();
+                return;
+
+            }
+
+        }
+
+        private int ValInt(object value)
+        {
+
+            if (value == null)
+            {
+                value = 0;
+            }
+
+            int v;
+            if (Int32.TryParse(value.ToString(), out v))
+            {
+                value = v;
+            }
+            else
+            {
+                value = 0;
+            }
+
+            return Int32.Parse(value.ToString());
+
+        }
+
+        private void Btn_Converter_Click(object sender, EventArgs e)
+        {
+
+            if (Txt_Numero.Text == "")
+            {
+
+                MessageBox.Show("Informme o Número Romano que deseja converter!", "Conversor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+
+            }
+
+            Txt_Resultado.Clear();
+
+            bool retorno = ValidaNumeroRomano();
+
+            if (retorno == false)
+            {
+
+                Txt_Numero.Clear();
+                Txt_Resultado.Clear();
+
+                MessageBox.Show("O número romano informado é inválido!", "Conversor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            int contador = 0;
+
+            if ((Txt_Numero.Text.Length - 1) == 0)
+            {
+
+                for (int i = 0; i <= _listaLetrasRomanas.Count - 1; i++)
+                {
+
+                    if (_listaLetrasRomanas[i] == Txt_Numero.Text.Substring(contador, 1).ToUpper())
+                    {
+
+                        Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                        break;
+
+                    }
+
+                }
+
+            }
+            else
+            {
+
+                while (contador <= Txt_Numero.Text.Length - 1)
+                {
+
+                    for (int i = 0; i <= _listaLetrasRomanas.Count - 1; i++)
+                    {
+
+                        if (_listaLetrasRomanas[i] == Txt_Numero.Text.Substring(contador, 1).ToUpper())
+                        {
+
+                            if ((_listaLetrasRomanas[i] == "V") || (_listaLetrasRomanas[i] == "X"))
+                            {
+
+                                if (contador != 0)
+                                {
+
+                                    if (Txt_Numero.Text.Substring(contador - 1, 1).ToUpper() == "I")
+                                    {
+                                        Txt_Resultado.Text = Convert.ToString((ValInt(Txt_Resultado.Text) - 2) + _listaNumerosInteiros[i]);
+
+                                        break;
+                                    }
+                                    else
+                                    {
+
+                                        Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                                        break;
+                                    }
+
+                                }
+                                else
+                                {
+
+                                    Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                                    break;
+                                }
+
+                            }
+
+                            else if ((_listaLetrasRomanas[i] == "L") || (_listaLetrasRomanas[i] == "C"))
+                            {
+                                if (contador != 0)
+                                {
+
+                                    if (Txt_Numero.Text.Substring(contador - 1, 1).ToUpper() == "X")
+                                    {
+                                        Txt_Resultado.Text = Convert.ToString((ValInt(Txt_Resultado.Text) - 20) + _listaNumerosInteiros[i]);
+
+                                        break;
+                                    }
+                                    else
+                                    {
+
+                                        Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                                        break;
+                                    }
+
+                                }
+                                else
+                                {
+
+                                    Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                                    break;
+                                }
+                            }
+
+                            else if ((_listaLetrasRomanas[i] == "D") || (_listaLetrasRomanas[i] == "M"))
+                            {
+                                if (contador != 0)
+                                {
+
+                                    if (Txt_Numero.Text.Substring(contador - 1, 1).ToUpper() == "C")
+                                    {
+                                        Txt_Resultado.Text = Convert.ToString((ValInt(Txt_Resultado.Text) - 200) + _listaNumerosInteiros[i]);
+
+                                        break;
+                                    }
+                                    else
+                                    {
+
+                                        Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                                        break;
+                                    }
+
+                                }
+                                else
+                                {
+
+                                    Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                                    break;
+                                }
+                            }
+
+                            else
+                            {
+
+                                Txt_Resultado.Text = Convert.ToString(ValInt(Txt_Resultado.Text) + _listaNumerosInteiros[i]);
+
+                                break;
+
+                            }
+
+                        }
+
+                    }
+
+                    contador += 1;
+                }
+
+            }
+
+        }
+
+        private bool ValidaNumeroRomano()
+        {
+
+            if (Txt_Numero.Text.ToUpper().Contains("IIII"))
+            {
+                return false;
+            }
+
+
+            if (Txt_Numero.Text.ToUpper().Contains("IIV"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("VV"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("VIIII"))
+            {
+                return false;
+            }
+
+
+            if (Txt_Numero.Text.ToUpper().Contains("VX"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("XXXX"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("IIX"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("XIIII"))
+            {
+                return false;
+            }
+
+
+            if (Txt_Numero.Text.ToUpper().Contains("IL"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("LIIII"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("VL"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("LL"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("LVV"))
+            {
+                return false;
+            }
+
+
+            if (Txt_Numero.Text.ToUpper().Contains("IC"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("CIIII"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("VC"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("CVV"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("LC"))
+            {
+                return false;
+            }
+
+
+            if (Txt_Numero.Text.ToUpper().Contains("CCCC"))
+            {
+                return false;
+            }
+
+
+            if (Txt_Numero.Text.ToUpper().Contains("CCD"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("ID"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("VD"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("LD"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("DD"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("DCCCC"))
+            {
+                return false;
+            }
+
+
+            if (Txt_Numero.Text.ToUpper().Contains("MMMM"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("IM"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("VM"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("XM"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("LM"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("DM"))
+            {
+                return false;
+            }
+            if (Txt_Numero.Text.ToUpper().Contains("MIIII"))
+            {
+                return false;
+            }
+                       
+            return true;
+
+        }
+
+    }
+
+}
+
